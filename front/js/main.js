56df1a7815131c680772e5c964c0bbea7f84edbb
Maksym Vikolash <vikolashmaxim@gmail.com>
2025-03-03 15:04:28 +0200

    linter applied and gitignore changes

--- front/js/main.js
+++ front/js/main.js
@@ -1,230 +1,226 @@
-(function (){
-    document.addEventListener("DOMContentLoaded", () =>{
-        const slides = document.querySelectorAll(".race__item")
-        const slideMoveLeft = document.querySelector(".race__nav-left")
-        const slideMoveRight = document.querySelector(".race__nav-right")
-        const slideCounter = document.querySelector(".race__nav-counter")
-        const betTables = document.querySelectorAll(".bet__item")
-        const resultsFirst = document.querySelector(".results__first")
-        const resultsSecond = document.querySelector(".results__second")
-        const resultsThird = document.querySelector(".results__third")
+(function () {
+	document.addEventListener('DOMContentLoaded', () => {
+		const slides = document.querySelectorAll('.race__item');
+		const slideMoveLeft = document.querySelector('.race__nav-left');
+		const slideMoveRight = document.querySelector('.race__nav-right');
+		const slideCounter = document.querySelector('.race__nav-counter');
+		const betTables = document.querySelectorAll('.bet__item');
+		const resultsFirst = document.querySelector('.results__first');
+		const resultsSecond = document.querySelector('.results__second');
+		const resultsThird = document.querySelector('.results__third');
 
-        let currentRace = 3;
+		let currentRace = 3;
 
-        slides.forEach((slide, i) =>{
-            if(i < currentRace){
-                slide.classList.add("_done")
-            }
-            if(i > currentRace){
-                slide.classList.add("_lock")
-            }
-        })
+		slides.forEach((slide, i) => {
+			if (i < currentRace) {
+				slide.classList.add('_done');
+			}
+			if (i > currentRace) {
+				slide.classList.add('_lock');
+			}
+		});
 
+		let currentSlide = currentRace;
 
-        let currentSlide = currentRace
+		function updateSlider(index) {
+			slides.forEach((slide) => slide.classList.remove('_active'));
+			slides[index].classList.add('_active');
+			slideCounter.textContent = `${index + 1}/${slides.length}`;
+			betTables.forEach((table) => {
+				if (currentSlide > currentRace) {
+					table.classList.add('_lock');
+				}
+				if (currentSlide < currentRace) {
+					table.classList.add('_done');
+				}
+				if (currentSlide === currentRace) {
+					table.classList.remove('_lock', '_done');
+				}
+			});
+		}
 
+		function moveSlide(direction) {
+			const currentBolid = slides[currentSlide].querySelector('.race__bolid');
+			currentBolid.classList.add('_move');
+			let nextSlide = direction === 'next' ? currentSlide + 1 : currentSlide - 1;
+			if (nextSlide < 0) nextSlide = slides.length - 1;
+			if (nextSlide >= slides.length) nextSlide = 0;
 
+			const nextBolid = slides[nextSlide].querySelector('.race__bolid');
+			nextBolid.classList.add('_arrive');
 
-        function updateSlider(index) {
-            slides.forEach(slide => slide.classList.remove("_active"));
-            slides[index].classList.add("_active");
-            slideCounter.textContent = `${index + 1}/${slides.length}`;
-            betTables.forEach(table =>{
-                if(currentSlide > currentRace){
-                    table.classList.add("_lock")
-                }
-                if(currentSlide < currentRace){
-                    table.classList.add("_done")
-                }
-                if(currentSlide === currentRace){
-                    table.classList.remove("_lock", "_done")
-                }
-            })
-        }
+			slides[currentSlide].classList.add('_opacity');
 
-        function moveSlide(direction) {
-            const currentBolid = slides[currentSlide].querySelector(".race__bolid");
-            currentBolid.classList.add("_move");
-            let nextSlide = direction === "next" ? currentSlide + 1 : currentSlide - 1;
-            if (nextSlide < 0) nextSlide = slides.length - 1;
-            if (nextSlide >= slides.length) nextSlide = 0;
+			setTimeout(() => {
+				currentBolid.classList.remove('_move');
+				nextBolid.classList.remove('_arrive');
+				currentSlide = nextSlide;
 
-            const nextBolid = slides[nextSlide].querySelector(".race__bolid");
-            nextBolid.classList.add("_arrive");
+				updateSlider(currentSlide);
+				slides.forEach((slide) => {
+					slide.classList.remove('_opacity');
+				});
+			}, 1500);
+		}
 
+		slideMoveLeft.addEventListener('click', () => {
+			moveSlide('prev');
+			document.querySelector('.race__nav').style.pointerEvents = 'none';
+			setTimeout(() => {
+				document.querySelector('.race__nav').style.pointerEvents = 'initial';
+			}, 2000);
+		});
+		slideMoveRight.addEventListener('click', () => {
+			moveSlide('next');
+			document.querySelector('.race__nav').style.pointerEvents = 'none';
+			setTimeout(() => {
+				document.querySelector('.race__nav').style.pointerEvents = 'initial';
+			}, 2000);
+		});
 
-            slides[currentSlide].classList.add("_opacity")
+		updateSlider(currentSlide);
 
-            setTimeout(() => {
-                currentBolid.classList.remove("_move");
-                nextBolid.classList.remove("_arrive");
-                currentSlide = nextSlide;
+		function startCountdown(endTime) {
+			const daysEls = document.querySelectorAll('.timer__days-item');
+			const hoursEls = document.querySelectorAll('.timer__hours-item');
+			const minutesEls = document.querySelectorAll('.timer__minutes-item');
 
-                updateSlider(currentSlide);
-                slides.forEach(slide =>{
-                    slide.classList.remove("_opacity")
-                })
-            }, 1500);
-        }
+			function updateTimer() {
+				const now = new Date().getTime();
+				const timeLeft = endTime - now;
 
-        slideMoveLeft.addEventListener("click", () => {
-            moveSlide("prev")
-            document.querySelector(".race__nav").style.pointerEvents = "none"
-            setTimeout(()=>{
-                document.querySelector(".race__nav").style.pointerEvents = "initial"
-            }, 2000)
-        });
-        slideMoveRight.addEventListener("click", () => {
-            moveSlide("next")
-            document.querySelector(".race__nav").style.pointerEvents = "none"
-            setTimeout(()=>{
-                document.querySelector(".race__nav").style.pointerEvents = "initial"
-            }, 2000)
-        });
+				if (timeLeft <= 0) {
+					clearInterval(timerInterval);
+					updateDigits(daysEls, '00');
+					updateDigits(hoursEls, '00');
+					updateDigits(minutesEls, '00');
+					return;
+				}
 
-        updateSlider(currentSlide);
+				const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+				const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+				const minutes = Math.floor((timeLeft / (1000 * 60)) % 60);
 
+				updateDigits(daysEls, String(days).padStart(2, '0'));
+				updateDigits(hoursEls, String(hours).padStart(2, '0'));
+				updateDigits(minutesEls, String(minutes).padStart(2, '0'));
+			}
 
+			function updateDigits(elements, value) {
+				elements[0].textContent = value[0];
+				elements[1].textContent = value[1];
+			}
 
+			updateTimer();
+			const timerInterval = setInterval(updateTimer, 60000);
+		}
 
-        function startCountdown(endTime) {
-            const daysEls = document.querySelectorAll(".timer__days-item");
-            const hoursEls = document.querySelectorAll(".timer__hours-item");
-            const minutesEls = document.querySelectorAll(".timer__minutes-item");
+		const now = new Date();
+		const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59).getTime();
 
-            function updateTimer() {
-                const now = new Date().getTime();
-                const timeLeft = endTime - now;
+		startCountdown(endOfMonth);
 
-                if (timeLeft <= 0) {
-                    clearInterval(timerInterval);
-                    updateDigits(daysEls, "00");
-                    updateDigits(hoursEls, "00");
-                    updateDigits(minutesEls, "00");
-                    return;
-                }
+		function startSmoke(
+			carSelector,
+			smokeClass,
+			maxCount,
+			interval,
+			delay,
+			fadeTime,
+			removeDelay,
+			extraClass = '',
+			activeClass = false
+		) {
+			const cars = document.querySelectorAll(carSelector);
+			cars.forEach((car) => {
+				if (!car) return;
+				console.log(car.parentElement.parentElement.classList.contains('_active'));
 
-                const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-                const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-                const minutes = Math.floor((timeLeft / (1000 * 60)) % 60);
+				function createSmoke() {
+					if (activeClass) {
+						if (!car || !car.parentElement.parentElement.classList.contains('_active')) return;
+					}
+					if (car.querySelectorAll(`.${smokeClass}${extraClass ? `.${extraClass}` : ''}`).length < maxCount) {
+						const smoke = document.createElement('div');
+						smoke.classList.add(smokeClass);
+						if (extraClass) smoke.classList.add(extraClass);
+						car.appendChild(smoke);
+						setTimeout(() => smoke.classList.add('_opacity'), fadeTime);
+						setTimeout(() => smoke.remove(), fadeTime + removeDelay);
+					}
+				}
+				createSmoke();
+				setTimeout(createSmoke, delay);
+				setInterval(() => {
+					createSmoke();
+					setTimeout(createSmoke, delay);
+				}, interval);
+			});
+		}
 
-                updateDigits(daysEls, String(days).padStart(2, "0"));
-                updateDigits(hoursEls, String(hours).padStart(2, "0"));
-                updateDigits(minutesEls, String(minutes).padStart(2, "0"));
-            }
+		startSmoke('.race__bolid-car', 'race__bolid-smoke-front', 4, 900, 500, 200, 1600, '', true);
+		startSmoke('.race__bolid-car', 'race__bolid-smoke-back', 8, 900, 500, 200, 1600, '', true);
+		startSmoke('.race__bolid-car', 'race__bolid-smoke-back', 4, 900, 500, 200, 1600, '_large', true);
+		startSmoke('.welcome__pers-smoke-front', 'front-before', 4, 900, 500, 200, 1600, '', false);
+		startSmoke('.welcome__pers-smoke-back', 'front-before', 4, 900, 500, 200, 1600, '', false);
 
-            function updateDigits(elements, value) {
-                elements[0].textContent = value[0];
-                elements[1].textContent = value[1];
-            }
+		function setPopups(triggerButton, popupClass) {
+			const popupsContainer = document.querySelector('.popups');
+			const popup = document.querySelector(`.popups__item.${popupClass}`);
 
-            updateTimer();
-            const timerInterval = setInterval(updateTimer, 60000);
-        }
+			if (!triggerButton || !popup || !popupsContainer) return;
 
-        const now = new Date();
-        const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0, 23, 59).getTime();
+			triggerButton.addEventListener('click', () => {
+				popupsContainer.classList.remove('_opacity');
+				popupsContainer.classList.add(popupClass);
+				document.body.style.overflow = 'hidden';
+			});
+			const closeButton = popup.querySelector('.popups__item-close');
 
-        startCountdown(endOfMonth);
+			popupsContainer.addEventListener('click', (e) => {
+				if (e.target === popupsContainer || e.target === closeButton) {
+					closePopup();
+				}
+			});
 
-        function startSmoke(carSelector, smokeClass, maxCount, interval, delay, fadeTime, removeDelay, extraClass = '', activeClass = false) {
-            const cars = document.querySelectorAll(carSelector);
-            cars.forEach(car =>{
-                if (!car) return;
-                console.log(car.parentElement.parentElement.classList.contains("_active"))
+			function closePopup() {
+				popupsContainer.classList.add('_opacity');
+				popupsContainer.classList.remove(popupClass);
+				document.body.style.overflow = '';
+			}
+		}
 
-                function createSmoke() {
-                    if(activeClass){
-                        if (!car || !car.parentElement.parentElement.classList.contains("_active")) return;
-                    }
-                    if (car.querySelectorAll(`.${smokeClass}${extraClass ? `.${extraClass}` : ''}`).length < maxCount) {
-                        const smoke = document.createElement('div');
-                        smoke.classList.add(smokeClass);
-                        if (extraClass) smoke.classList.add(extraClass);
-                        car.appendChild(smoke);
-                        setTimeout(() => smoke.classList.add("_opacity"), fadeTime);
-                        setTimeout(() => smoke.remove(), fadeTime + removeDelay);
-                    }
-                }
-                createSmoke();
-                setTimeout(createSmoke, delay);
-                setInterval(() => {
-                    createSmoke();
-                    setTimeout(createSmoke, delay);
-                }, interval);
-            })
+		setPopups(document.querySelector('.bet__btn-item'), '_betPopup');
+		setPopups(document.querySelector('.confirm__upd-btn'), '_confirmPopup');
+		setPopups(document.querySelector('.results__popup-btn'), '_resultsPopup');
 
-        }
+		function animateOnScroll(element, animationClass, delay) {
+			const options = {
+				root: null,
+				rootMargin: '0px',
+				threshold: 0.2,
+			};
 
-        startSmoke('.race__bolid-car', 'race__bolid-smoke-front', 4, 900, 500, 200, 1600, "", true);
-        startSmoke('.race__bolid-car', 'race__bolid-smoke-back', 8, 900, 500, 200, 1600, "", true);
-        startSmoke('.race__bolid-car', 'race__bolid-smoke-back', 4, 900, 500, 200, 1600, "_large", true);
-        startSmoke('.welcome__pers-smoke-front', 'front-before', 4, 900, 500, 200, 1600, "", false);
-        startSmoke('.welcome__pers-smoke-back', 'front-before', 4, 900, 500, 200, 1600, "", false);
+			const observer = new IntersectionObserver((entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						setTimeout(() => {
+							entry.target.classList.add(animationClass);
+						}, delay);
+					} else {
+						entry.target.classList.remove(animationClass);
+					}
+				});
+			}, options);
 
+			observer.observe(element);
+		}
 
-        function setPopups(triggerButton, popupClass) {
-            const popupsContainer = document.querySelector('.popups');
-            const popup = document.querySelector(`.popups__item.${popupClass}`);
-
-            if (!triggerButton || !popup || !popupsContainer) return;
-
-            triggerButton.addEventListener('click', () => {
-                popupsContainer.classList.remove('_opacity');
-                popupsContainer.classList.add(popupClass);
-                document.body.style.overflow = 'hidden';
-            });
-            const closeButton = popup.querySelector('.popups__item-close');
-
-            popupsContainer.addEventListener("click", (e) =>{
-                if (e.target === popupsContainer || e.target === closeButton){
-                    closePopup()
-                }
-            })
-
-            function closePopup() {
-                popupsContainer.classList.add('_opacity');
-                popupsContainer.classList.remove(popupClass);
-                document.body.style.overflow = '';
-            }
-        }
-
-        setPopups(document.querySelector('.bet__btn-item'), '_betPopup');
-        setPopups(document.querySelector('.confirm__upd-btn'), '_confirmPopup');
-        setPopups(document.querySelector('.results__popup-btn'), '_resultsPopup');
-
-        function animateOnScroll(element, animationClass, delay) {
-            const options = {
-                root: null,
-                rootMargin: '0px',
-                threshold: 0.2
-            };
-
-            const observer = new IntersectionObserver((entries) => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        setTimeout(() =>{
-                            entry.target.classList.add(animationClass);
-                        }, delay)
-
-                    } else {
-                        entry.target.classList.remove(animationClass);
-                    }
-                });
-            }, options);
-
-            observer.observe(element);
-
-
-        }
-
-        animateOnScroll(resultsFirst.querySelector(".results__top-decor"), "_show", 0)
-        animateOnScroll(resultsSecond.querySelector(".results__top-decor"), "_show", 400)
-        animateOnScroll(resultsThird.querySelector(".results__top-decor"), "_show", 800)
-        animateOnScroll(resultsFirst.querySelector(".results__top-wrap"), "_show", 0)
-        animateOnScroll(resultsSecond.querySelector(".results__top-wrap"), "_show", 400)
-        animateOnScroll(resultsThird.querySelector(".results__top-wrap"), "_show", 800)
-
-
-    })
-})()
+		animateOnScroll(resultsFirst.querySelector('.results__top-decor'), '_show', 0);
+		animateOnScroll(resultsSecond.querySelector('.results__top-decor'), '_show', 400);
+		animateOnScroll(resultsThird.querySelector('.results__top-decor'), '_show', 800);
+		animateOnScroll(resultsFirst.querySelector('.results__top-wrap'), '_show', 0);
+		animateOnScroll(resultsSecond.querySelector('.results__top-wrap'), '_show', 400);
+		animateOnScroll(resultsThird.querySelector('.results__top-wrap'), '_show', 800);
+	});
+})();
